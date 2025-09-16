62536daeabeee867a6472d48c68f2afdc172fc61
omchy34 <omchy34@gmail.com>
2025-09-16 21:21:38 +0530

    done

--- ecom-clone/src/app/store.js
+++ ecom-clone/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from '../features/AddToCart/AddToCart'
-import wishlistReducer from '../features/wishlist/WishList'
+import wishlistReducer from '../features/wishlist/WishList.js'
 import authReducer from '../features/auth/authSlice'
 
 const store = configureStore({
