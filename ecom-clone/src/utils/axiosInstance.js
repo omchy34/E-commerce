fe358075dbf535266713af6480fd5038003bc437
Om Choudhary <76719323+omchy34@users.noreply.github.com>
2025-09-28 08:31:17 +0000

    Update axiosInstance base URL to use production server for API requests

--- ecom-clone/src/utils/axiosInstance.js
+++ ecom-clone/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5673', // Replace with your API base URL
+  baseURL: 'https://e-commerceserver-uu0f.onrender.com', // Replace with your API base URL
   withCredentials: true, // Allow cookies to be sent with requests
 });
 
