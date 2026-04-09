60ba0059eb03e25a7e85e34a31839d9591be48a1
Om Choudhary <76719323+omchy34@users.noreply.github.com>
2026-04-09 12:03:05 +0530

    Comment out withCredentials in axiosInstance
    Commented out the withCredentials option in axios instance.

--- ecom-clone/src/utils/axiosInstance.js
+++ ecom-clone/src/utils/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'https://e-commerceserver-uu0f.onrender.com', // Replace with your API base URL
-  withCredentials: true, // Allow cookies to be sent with requests
+  // withCredentials: true, // Allow cookies to be sent with requests
 });
 
 export default axiosInstance;
